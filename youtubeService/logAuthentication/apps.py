648a7f3a867565a6e58be0f981e3b04d699ba662
amin <amin603123@gmail.com>
2019-06-22 08:17:19 +0300

    editing the files form (fixing last submit problem)

--- youtubeService/logAuthentication/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class LogauthenticationConfig(AppConfig):
-    name = 'logAuthentication'
--- youtubeService/page404/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class Page404Config(AppConfig):
-    name = 'page404'
